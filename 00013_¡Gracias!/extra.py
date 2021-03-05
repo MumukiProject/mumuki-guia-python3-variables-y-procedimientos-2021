faaf299497050eedcbbb1cda5e2d893e5cf15245
gustavo@mumuki.org <gustavo@mumuki.org>
2021-03-05 20:56:32 +0000

    Mumuki Export on 2021-03-05 20:56:32 +0000

--- 00013_¡Gracias!/extra.py
+++ 00013_¡Gracias!/extra.py
@@ -2,10 +2,13 @@ agua_del_mate = 0
 agua_del_termo = 1000
 
 def cebar_mate():
+  global agua_del_mate
+  global agua_del_termo
   agua_del_termo -= 30
   agua_del_mate += 30
   
 def tomar_mate():
+  global agua_del_mate
   agua_del_mate = 0
   
 def pasar():
