4e59bea4945c0cc293b580e578065059b4fe0c6a
gustavo@mumuki.org <gustavo@mumuki.org>
2021-03-10 17:52:28 +0000

    Mumuki Export on 2021-03-10 17:52:28 +0000

--- 00011_No creo tomar mucho más/test.py
+++ 00011_No creo tomar mucho más/test.py
@@ -1,12 +1,12 @@
   
-  def test_cargar_termo(200)_aumenta_en_200_el_agua_del_termo(self):
+  def test_cargar_termo_con_200_como_argumento_aumenta_en_200_el_agua_del_termo(self):
     global agua_del_termo
     agua_del_termo = 600
     cargar_termo(200)
     self.assertEqual(agua_del_termo, 800)
 
 
-  def test_cargar_termo(500)_aumenta_en_500_el_agua_del_termo(self):
+  def test_cargar_termo_con_500_como_argumento_aumenta_en_500_el_agua_del_termo(self):
     global agua_del_termo
     agua_del_termo = 200
     cargar_termo(500)
