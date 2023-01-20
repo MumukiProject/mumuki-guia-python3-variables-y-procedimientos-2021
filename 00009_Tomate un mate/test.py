9397152cc5c019dfeb70683336a2b30beae6f882
franco@mumuki.org <franco@mumuki.org>
2023-01-20 17:01:46 +0000

    Mumuki Export on 2023-01-20 17:01:46 +0000

--- 00009_Tomate un mate/test.py
+++ 00009_Tomate un mate/test.py
@@ -1,4 +1,7 @@
   
+  def test_inicialmente_el_agua_del_termo_esta_en_1000(self):
+    self.assertEqual(agua_del_termo, 1000, "el agua del termo debe inicializarse en 1000")
+
   def test_cebar_mate_disminuye_en_30_ml_el_agua_del_termo(self):
     global agua_del_termo
     agua_del_termo = 1000
@@ -13,3 +16,6 @@
     cebar_mate()
     cebar_mate()
     self.assertEqual(agua_del_termo, 910)
+    
+    
+      
