4418b830b2909372ae855e1d0639d0d3b1e0703d
franco@mumuki.org <franco@mumuki.org>
2023-01-20 17:07:37 +0000

    Mumuki Export on 2023-01-20 17:07:37 +0000

--- 00010_Se enfrió el agua/test.py
+++ 00010_Se enfrió el agua/test.py
@@ -1,10 +1,30 @@
   
-  def test_vaciar_termo_deja_en_0_el_agua_del_termo(self):
+  def test_vaciar_termo_deja_en_0_el_agua_del_termo_si_tenia_500(self):
+    global agua_del_termo
+    agua_del_termo = 500
+    vaciar_termo()
+    self.assertEqual(agua_del_termo, 0)
+
+  
+  def test_vaciar_termo_deja_en_0_el_agua_del_termo_si_tenia_200(self):
+    global agua_del_termo
+    agua_del_termo = 200
+    vaciar_termo()
+    self.assertEqual(agua_del_termo, 0)
+
+
+  def test_vaciar_termo_deja_en_0_el_agua_del_termo_si_tenia_1000(self):
     global agua_del_termo
     agua_del_termo = 1000
     vaciar_termo()
     self.assertEqual(agua_del_termo, 0)
 
+
+  def test_vaciar_termo_sigue_dejando_en_0_el_agua_si_ya_estaba_en_0(self):
+    global agua_del_termo
+    agua_del_termo = 0
+    vaciar_termo()
+    self.assertEqual(agua_del_termo, 0)
 
   def test_llenar_termo_deja_en_1000_el_agua_del_termo(self):
     global agua_del_termo
